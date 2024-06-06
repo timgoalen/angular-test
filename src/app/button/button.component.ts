a8b5c707a036dcbecc16d58fded1d76d6558dbdf
Tim Goalen <timgoalen@hotmail.com>
2024-06-06 08:40:20 +0000

    chore: set up empty angular app

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  handleBtnClick() {
+    alert("clicked");
+  }
+}
